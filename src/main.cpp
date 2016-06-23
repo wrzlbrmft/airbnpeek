ef30034b96ea97bc4b12244950c312af2808fea7
matthias <matthias@mucqmaws092>
2016-06-23 10:56:56 +0200

    processing of standard cli options

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 
-using namespace std;
 using namespace boost;
 
+#define APP_NAME "airbnpeek"
+#define APP_VERSION "0.0.0"
+
+void print_version_info() {
+	std::cout << APP_NAME << " " << APP_VERSION << std::endl;
+}
+
+void print_help_message(program_options::options_description &desc) {
+	print_version_info();
+	std::cout << desc << std::endl;
+}
+
 int main(int argc, char** argv) {
-	program_options::options_description desc("options");
-	desc.add_options()
-		("help,h", "print this help message and exit");
+	try {
+		program_options::options_description desc("options");
+		desc.add_options()
+			("version", "print version info and exit")
+			("help,h", "print this help message and exit");
 
-	program_options::variables_map vm;
-	program_options::store(program_options::parse_command_line(argc, argv, desc), vm);
-	program_options::notify(vm);
+		program_options::variables_map vm;
+		program_options::store(program_options::parse_command_line(argc, argv, desc), vm);
+		program_options::notify(vm);
 
-	if (vm.count("help")) {
-		cout << desc << endl;
-		return 0;
+		if (vm.count("version")) {
+			print_version_info();
+			return 0;
+		}
+
+		if (vm.count("help")) {
+			print_help_message(desc);
+			return 0;
+		}
+	}
+	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
+	catch (...) {
+		std::cerr << "unknown error" << std::endl;
+		return -1;
 	}
 
-	cout << "airbnpeek" << endl;
 	return 0;
 }
